ca18cfbccbc0a57dac66eecc22e3bbd845711d09
Sam948-byte <techguy763@gmail.com>
2023-07-19 16:13:43 -0500

    lots of stuff

--- Focus_session_1_files/GUIs/src/HelloWorld.java
+++ Focus_session_1_files/GUIs/src/HelloWorld.java
@@ -8,7 +8,24 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class HelloWorld{
+public class HelloWorld extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label message = new Label("Hello, World!\n I'm a label!");
+        message.setFont(new Font("Comic Sans", 100));
+        message.setTextFill(Color.RED);
 
+        Button button = new Button("Click me!");
+        button.setOnAction((ActionEvent event) -> {
+            message.setText("Goodbye, World!");
+        });
 
+        HBox root = new HBox();
+        root.getChildren().addAll(message, button);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Hello, World!");
+        stage.show();
+    }
 }
